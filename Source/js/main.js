0139d3e502562fe954d148df989b4ee03bab727d
PrincessGod <jihongjiang@outlook.com>
2017-08-01 18:43:00 +0800

    Config requirejs

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,16 +1,24 @@
-require.config({
-	paths: {
-		'Cesium': '../../Build/Cesium/Cesium'
-	},
-	shim: {
-		Cesium: {
-			exports: 'Cesium'
-		}
-	}
-});
+var developMode = true;
 
-if(typeof Cesium !== "undefined") {
-	onload(Cesium);
-} else if(typeof require === "function") {
-	require(["Cesium"], onload);
+if (developMode) {
+    require.config({
+    	baseUrl: '../../Cesium/Source'
+    });
+} else {
+    require.config({
+        paths: {
+            'Cesium': '../../Cesium/Build/Cesium/Cesium'
+        },
+        shim: {
+            Cesium: {
+                exports: 'Cesium'
+            }
+        }
+    });
+}
+
+if (typeof Cesium !== "undefined") {
+    onload(Cesium);
+} else if (typeof require === "function") {
+    require(["Cesium"], onload);
 }
