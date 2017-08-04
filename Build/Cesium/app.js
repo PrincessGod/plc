a16d1fe02b590e4b2c50685828ee389100b0caf5
PrincessGod <jihongjiang@outlook.com>
2017-08-04 13:53:31 +0800

    Minify and remove widgets

---
+++ Build/Cesium/app.js
@@ -0,0 +1,28 @@
+    require(['plc'], function() {
+        'user strict';
+
+        document.getElementById('loading-container').style.display = 'none';
+
+        var viewer = new Cesium.Viewer('cesiumContainer', {
+            // Wedgets
+            animation: false,
+            timeline: false,
+            fullscreenButton: false,
+            geocoder: false,
+            homeButton: false,
+            navigationHelpButton: false,
+            baseLayerPicker: false,
+            sceneModePicker: true,
+            infoBox: false,
+            selectionIndicator: false,
+            creditContainer: document.createElement('DIV'),
+
+            // Display
+            imageryProvider: Cesium.createTileMapServiceImageryProvider({
+                url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+            }),
+            terrainProviderViewModels: undefined,
+            terrainProvider: new Cesium.EllipsoidTerrainProvider(),
+            sceneMode: Cesium.SceneMode.SCENE3D // Cesium.SceneMode.SCENE2D Cesium.SceneMode.COLUMBUS_VIEW
+        });
+    });
