58cb63e74b9c39b30840f9975ea69c8d67d4b6be
PrincessGod <jihongjiang@outlook.com>
2017-08-14 12:03:07 +0800

    Fix imagery layer bug

--- Build/Cesium/app.js
+++ Build/Cesium/app.js
@@ -1,39 +1,43 @@
-    require(['plc'], function() {
-        'user strict';
+requirejs.config({
+    waitSeconds: 200
+});
 
-        document.getElementById('loading-container').style.display = 'none';
+require(['plc'], function() {
+    'user strict';
 
-        var viewer = new Cesium.Viewer('cesiumContainer', {
-            // Wedgets
-            animation: false,
-            timeline: false,
-            fullscreenButton: false,
-            geocoder: false,
-            homeButton: false,
-            navigationHelpButton: false,
-            baseLayerPicker: false,
-            sceneModePicker: true,
-            infoBox: false,
-            selectionIndicator: false,
-            creditContainer: document.createElement('DIV'),
+    document.getElementById('loading-container').style.display = 'none';
 
-            // Display
-            imageryProvider: Cesium.createTileMapServiceImageryProvider({
-                url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
-            }),
-            terrainProviderViewModels: undefined,
-            terrainProvider: new Cesium.EllipsoidTerrainProvider(),
-            sceneMode: Cesium.SceneMode.SCENE3D // Cesium.SceneMode.SCENE2D Cesium.SceneMode.COLUMBUS_VIEW
-        });
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        // Wedgets
+        animation: false,
+        timeline: false,
+        fullscreenButton: false,
+        geocoder: false,
+        homeButton: false,
+        navigationHelpButton: false,
+        baseLayerPicker: false,
+        sceneModePicker: true,
+        infoBox: false,
+        selectionIndicator: false,
+        creditContainer: document.createElement('DIV'),
 
-        viewer.camera.setView({
-            destination: Cesium.Cartesian3.fromDegrees(114.0, 37, 20000000),
-            orientation: {
-                heading: 0.0,
-                pitch: -Cesium.Math.PI_OVER_TWO,
-                roll: 0.0
-            }
-        });
-
-        var compass = new Cesium.PLC.CompassButton(viewer._toolbar, viewer.scene);
+        // Display
+        imageryProvider: Cesium.createTileMapServiceImageryProvider({
+            url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+        }),
+        terrainProviderViewModels: undefined,
+        terrainProvider: new Cesium.EllipsoidTerrainProvider(),
+        sceneMode: Cesium.SceneMode.SCENE3D // Cesium.SceneMode.SCENE2D Cesium.SceneMode.COLUMBUS_VIEW
     });
+
+    viewer.camera.setView({
+        destination: Cesium.Cartesian3.fromDegrees(114.0, 37, 20000000),
+        orientation: {
+            heading: 0.0,
+            pitch: -Cesium.Math.PI_OVER_TWO,
+            roll: 0.0
+        }
+    });
+
+    var compass = new Cesium.PLC.CompassButton(viewer._toolbar, viewer.scene);
+});
