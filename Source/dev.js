06ed41edd4874e7679f764785ae4f2bbaf75f6bb
PrincessGod <jihongjiang@outlook.com>
2017-08-15 16:50:09 +0800

    Add line measure sample

--- Source/dev.js
+++ Source/dev.js
@@ -1,37 +1,38 @@
-require.config({
-    paths: {
-        'plcjs': './Plc'
-    }
-});
+  require.config({
+      paths: {
+          'plcjs': './Plc'
+      }
+  });
 
 
-if (typeof Cesium !== "undefined") {
-    console.log("Cesium should not be defined at this time.");
-} else if (typeof require === "function") {
-    require(['plcjs/plc'], function(Cesium) {
-        this.viewer = new Cesium.Viewer('cesiumContainer', {
-            // Wedgets
-            animation: false,
-            timeline: false,
-            fullscreenButton: false,
-            geocoder: false,
-            homeButton: false,
-            navigationHelpButton: false,
-            baseLayerPicker: false,
-            sceneModePicker: true,
-            infoBox: false,
-            selectionIndicator: false,
-            creditContainer: document.createElement('DIV'),
+  if (typeof Cesium !== 'undefined') {
+      console.log('Cesium should not be defined at this time.');
+  } else if (typeof require === 'function') {
+      require(['plcjs/plc'], function (Cesium) {
+          this.viewer = new Cesium.Viewer('cesiumContainer', {
+              // Wedgets
+              animation: false,
+              timeline: false,
+              fullscreenButton: false,
+              geocoder: false,
+              homeButton: false,
+              navigationHelpButton: false,
+              baseLayerPicker: false,
+              sceneModePicker: true,
+              infoBox: false,
+              selectionIndicator: false,
+              creditContainer: document.createElement('DIV'),
 
-            // Display
-            imageryProvider: Cesium.createTileMapServiceImageryProvider({
-                url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
-            }),
-            terrainProviderViewModels: undefined,
-            terrainProvider: new Cesium.EllipsoidTerrainProvider(),
-            sceneMode: Cesium.SceneMode.SCENE3D // Cesium.SceneMode.SCENE2D Cesium.SceneMode.COLUMBUS_VIEW
-        });
-        this.Cesium = Cesium;
-        var compass = new Cesium.PLC.CompassButton(viewer._toolbar, viewer.scene);
-    });
-}
+              // Display
+              imageryProvider: Cesium.createTileMapServiceImageryProvider({
+                  url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+              }),
+              terrainProviderViewModels: undefined,
+              terrainProvider: new Cesium.EllipsoidTerrainProvider(),
+              sceneMode: Cesium.SceneMode.SCENE3D // Cesium.SceneMode.SCENE2D Cesium.SceneMode.COLUMBUS_VIEW
+          });
+          this.Cesium = Cesium;
+          var compass = new Cesium.PLC.CompassButton(viewer._toolbar, viewer.scene);
+      });
+
+  }
