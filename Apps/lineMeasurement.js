e0c7eb084a0bc3342e99268ed8568a8a6910dacc
PrincessGod <jihongjiang@outlook.com>
2017-08-16 15:47:57 +0800

    Update line measure sample with new vhMeasure

--- Apps/lineMeasurement.js
+++ Apps/lineMeasurement.js
@@ -51,11 +51,67 @@ require(['plc'], function () {
 
     goHome();
 
-    Sandcastle.addToolbarButton('Start Measurement', function () {
+    Sandcastle.addToolbarButton('Start Measure', function () {
         lineMeasure.startDraw();
-    });
-    Sandcastle.addToolbarButton('End Measurement', function () {
+    }, 'toolbarbuttons');
+    Sandcastle.addToolbarButton('End Measure', function () {
         lineMeasure.endDraw();
-    });
-    Sandcastle.addToolbarButton('Home', goHome);
+    }, 'toolbarbuttons');
+    Sandcastle.addToggleButton('ShowVH', false, function (checked) {
+        lineMeasure.vhMeasure = checked;
+    }, 'toolbarbuttons');
+
+    var lastEntity = {};
+
+    function createModel(url, height) {
+        viewer.entities.remove(lastEntity);
+
+        var cartegrao = Cesium.Cartographic.fromCartesian(new Cesium.Cartesian3(-2378169.0658539943, 4457736.679267226, 3884743.8028813666));
+        var position = Cesium.Cartesian3.fromRadians(cartegrao.longitude, cartegrao.latitude, height);
+        var heading = Cesium.Math.toRadians(135);
+        var pitch = 0;
+        var roll = 0;
+        var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+        var orientation = Cesium.Transforms.headingPitchRollQuaternion(position, hpr);
+
+        lastEntity = viewer.entities.add({
+            name: url,
+            position: position,
+            orientation: orientation,
+            model: {
+                uri: url
+            }
+        });
+        viewer.trackedEntity = lastEntity;
+    }
+
+    var options = [{
+        text: 'Aircraft',
+        onselect: function () {
+            createModel('../models/CesiumAir/Cesium_Air.glb', 5000.0);
+        }
+    }, {
+        text: 'Ground vehicle',
+        onselect: function () {
+            createModel('../models/CesiumGround/Cesium_Ground.glb', 0);
+        }
+    }, {
+        text: 'Hot Air Balloon',
+        onselect: function () {
+            createModel('../models/CesiumBalloon/CesiumBalloon.glb', 1000.0);
+        }
+    }, {
+        text: 'Milk truck',
+        onselect: function () {
+            createModel('../models/CesiumMilkTruck/CesiumMilkTruck-kmc.glb', 0);
+        }
+    }, {
+        text: 'Skinned character',
+        onselect: function () {
+            createModel('../models/CesiumMan/Cesium_Man.glb', 0);
+        }
+    }];
+
+    Sandcastle.addToolbarMenu(options, 'toolbarmenu');
+    Sandcastle.addToolbarButton('Home', goHome, 'toolbarmenu');
 });
